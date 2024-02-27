9d3edb539834784ce47a88106bbe72ec26af5e4b
xieyuxing <185246042@qq.com>
2024-02-28 03:50:42 +0800

    2

--- bin/dartstudy.dart
+++ bin/dartstudy.dart
@@ -1,20 +1,11 @@
-void main() {
-
-  var print1 = setDiscountToCalTotal(0.8);
-  print1(100);
-  var print2 = setDiscountToCalTotal(0.5);
-  print1(50);
-  print2(500);
-  print(total);
+Iterable<int> naturalsTo(int n) sync* {
+  int k = 0;
+  while (k < n) yield k++;
 }
 
-double total=0;
-Function setDiscountToCalTotal(double discount){
-  print('设置折扣为：$discount');
-  var printTotal = (int price){
-    double cost = price*discount;
-    total+=cost;
-    print('当前商品价格：$price,折扣：${discount},折后价：$cost,总价：$total');
-  };
-  return printTotal;
+void main() {
+  Iterable<int> list = naturalsTo(1);
+  naturalsTo(2);
+  naturalsTo(3);
+  print(list);
 }
