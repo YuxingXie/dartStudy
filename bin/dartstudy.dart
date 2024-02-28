2be25a44fd38f0c74b5ce19412fbef129dd12e11
xieyuxing <185246042@qq.com>
2024-02-28 16:04:27 +0800

    2

--- bin/dartstudy.dart
+++ bin/dartstudy.dart
@@ -1,12 +1,26 @@
-Iterable<int> naturalsTo(int n) sync* {
-  int k = 0;
-  while (k < n) yield k++;
-}
+
 
 void main() {
-
-  print(naturalsDownFrom(8));
-
+  int age=-1;
+  String? x;
+  x=switch(age){
+    >=0&&<3=>
+      '$age years old is baby' ,
+    >=3&&<=10
+      =>'$age years old is kid',
+    (>10&&<18)
+  =>'$age years old is teenage',
+    (>=18&&<60)
+  =>'$age years old is yong person',
+    (>=60&&<150)
+  =>'$age years old is old person',
+    (>=150)
+  =>'$age years old is not a person',
+//  _ => throw FormatException('Invalid')
+  _ => 'bad age format'
+//  <0 => 'bad age format'
+  };
+  print(x);
 
 
 }
