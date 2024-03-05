87d2ace961ba1a8d6cd6573033369564bd2f32c6
xieyuxing <185246042@qq.com>
2024-03-06 05:52:43 +0800

    22

--- src/main/dart/c11_Async/AwaitAndAsync.dart
+++ src/main/dart/c11_Async/AwaitAndAsync.dart
@@ -1,7 +1,17 @@
-Future<void> checkVersion() async {
+//void checkVersion() async {
+Future<String> checkVersion() async {
   var version = await lookUpVersion();
+  print(version);
   // Do something with version
+//  return  null as Future<String>;
+  return  Future<String>(version);
 }
 
 lookUpVersion() {
+
+}
+
+main(){
+  Future<String> f = checkVersion();
+  print(f);
 }
