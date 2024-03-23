a2ddad1f38aefad0f023303487275649ce752a94
xieyuxing <185246042@qq.com>
2024-03-23 19:41:00 +0800

    22

--- src/main/dart/c4_collections_api/map_api.dart
+++ src/main/dart/c4_collections_api/map_api.dart
@@ -1,4 +1,8 @@
 main() {
-  var map = Map<String, dynamic>.from({'a': '100 dollors', 'b': 100});
-  print(map['a'] == '100 dollors');
+  var mapDynamic = Map<String, dynamic>.of({'a': '100 dollors', 'b': 100});
+  var mapObj = Map<String, Object>.of({'a': '100 dollors', 'b': 100});
+  var map1 = Map<String, dynamic>.of(mapObj);
+  // var map2 = Map<String, Object>.of(mapDynamic);
+
+  // var map = Map<String, Object>.of({'a': '100 dollors', 'b': 100});
 }
