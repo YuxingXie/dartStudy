e55dc328c171d092ba106c7db48b7a412ed2905d
xieyuxing <185246042@qq.com>
2024-03-23 23:27:01 +0800

    333

--- src/main/dart/c4_collections_api/map_api.dart
+++ src/main/dart/c4_collections_api/map_api.dart
@@ -5,4 +5,14 @@ main() {
   // var map2 = Map<String, Object>.of(mapDynamic);
 
   // var map = Map<String, Object>.of({'a': '100 dollors', 'b': 100});
+  Map<String, int> hist = {
+    'a': 23,
+    'b': 100,
+  };
+
+  for (var MapEntry(value: v, key: k) in hist.entries) {
+    print('$k occurred $v times');
+  }
+  var [a, b] = [1, 2];
+  print(a + b);
 }
